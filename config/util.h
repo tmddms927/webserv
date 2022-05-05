ad841169fdd4356df918a84c9d34511c6f875e19
gilee <weg901127@naver.com>
2022-05-05 22:24:27 +0900

    [SUP-40][fix, add]    - location block 파싱 추가    - ServerBlock.cpp 추가    - server_s 구조체에 변수 추가

---
+++ config/util.h
@@ -0,0 +1,21 @@
+//
+// Created by gilee on 2022/05/05.
+//
+
+#ifndef UNTITLED1_UTIL_H
+#define UNTITLED1_UTIL_H
+#include "constant.h"
+
+typedef struct  global_s {
+    std::string err_page;
+    int         client_max_body_size;
+}               global;
+
+typedef struct  servers_s {
+    std::string host;
+    std::string location;
+    std::string cgi;
+    std::string root;
+    int         port;
+}               servers;
+#endif //UNTITLED1_UTIL_H
