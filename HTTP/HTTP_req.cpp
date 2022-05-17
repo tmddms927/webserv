1d6c64dbbb3dc02630d0e9b3823accfb949927e6
Jeonghwan Kim <jeokim@c3r4s7.42seoul.kr>
2022-05-17 10:39:20 +0900

    [fix] fixing

--- HTTP/HTTP_req.cpp
+++ HTTP/HTTP_req.cpp
@@ -92,7 +92,7 @@ bool    HTTP::parseRequestBody() {
         requestMessage.buf = "";            //  non_body일 경우, 모든 body를 버리기
 
     if (requestMessage.body.size() > REQUEST_BODY_MAX_SIZE)
-        setStatus(PATLOAD_TOO_LARGE);       //  body_len이 REQUEST_BODY_MAX_SIZE보다 크면 error
+        setStatus(PATLOAD_TOO_LARGE);
 
     if (requestMessage.content_length >= 0) //  Content-Length, Transfer-Encoding 모두 있을 경우
         ret = reqBodyContentLength();       //  Content-Length를 우선함
@@ -228,19 +228,19 @@ bool HTTP::reqCheckHeaderFinished() {
 /*
 ** request message print
 */
-void HTTP::reqPrint() {
-    std::cout << "==============<<  request parsing finish  >>==============" << std::endl; 
-    std::cout << requestMessage.method_name << " " << requestMessage.unparsed_uri << std::endl;
-    for (std::map<std::string, std::string>::iterator it = requestMessage.header_in.begin(); it != requestMessage.header_in.end(); ++it)
-        std::cout << it->first << ": " << it->second << std::endl;
-    // if (!requestMessage.non_body)
-    //     std::cout << requestMessage.body << "<----------BODY END]" << std::endl;
-    // else
-    //     std::cout << "************* there is no body ***************" << std::endl;
-    std::cout << "port_num : " << requestMessage.port_num << std::endl
-                << "keep-alive : " << requestMessage.keep_alive << std::endl
-                << "content_length : " << requestMessage.content_length << std::endl
-                << "chunked : " << requestMessage.chunked << std::endl
-                << "method : " << static_cast<int>(requestMessage.method) << std::endl;
-    std::cout << "==============<< ststus : " << status << " >>================" << std::endl;
-}
+// void HTTP::reqPrint() {
+//     std::cout << "==============<<  request parsing finish  >>==============" << std::endl; 
+//     std::cout << requestMessage.method_name << " " << requestMessage.unparsed_uri << std::endl;
+//     for (std::map<std::string, std::string>::iterator it = requestMessage.header_in.begin(); it != requestMessage.header_in.end(); ++it)
+//         std::cout << it->first << ": " << it->second << std::endl;
+//     // if (!requestMessage.non_body)
+//     //     std::cout << requestMessage.body << "<----------BODY END]" << std::endl;
+//     // else
+//     //     std::cout << "************* there is no body ***************" << std::endl;
+//     std::cout << "port_num : " << requestMessage.port_num << std::endl
+//                 << "keep-alive : " << requestMessage.keep_alive << std::endl
+//                 << "content_length : " << requestMessage.content_length << std::endl
+//                 << "chunked : " << requestMessage.chunked << std::endl
+//                 << "method : " << static_cast<int>(requestMessage.method) << std::endl;
+//     std::cout << "==============<< ststus : " << status << " >>================" << std::endl;
+// }
